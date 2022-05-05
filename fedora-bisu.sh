785dc562dd125baeef90e4e9d807c2fdac28be94
BISHAL POUDEL <68413032+nepbish07al@users.noreply.github.com>
2022-05-05 18:25:16 +0545

    Add files via upload

---
+++ fedora-bisu.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+sudo dnf update && sudo dnf upgrade
+
+sudo dnf install sddm
+sudo systemctl enable sddm
+sudo systemctl set-default graphical.target
+
+sudo dnf install nano
+sudo dnf install git
+
+git clone https://github.com/nepbish07al/bisu-linux.git
+cd bisu-linux
+mkdir ~/.config
+cp bg.jpg ~/.config
+ls dotconfig/
+mv dotconfig/* ~/.config/
+sudo dnf install bspwm kitty picom polybar rofi sxhkd thunar
+cat .xinit
+mv .* ~
+
+cd rpm-packages/
+ls
+sudo dnf install ocs
+sudo dnf install ocs-url-3.1.0-1.fc20.x86_64.rpm
+
+sudo nano /etc/sddm.conf
+echo 'set up your login page, login session to bspwm and user to ='
+
+sudo dnf install fontawesome-fonts fontawesome-fonts-web
+
+sudo dnf install firefox
+sudo dnf install arandar 
+echo 'def resolution'
+echo 'instal nerdfonts -firecoda,meslo and make .fonts directory and unzip those'
+echo 'install network manager and install wireless driver'
+echo 'etc/sddm.conf'
+
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+#sudo rpm -i google-chrome-stable_current_x86_64.rpm
+#sudo rpm -i google-chrome-stable_current_amd64.deb
+
