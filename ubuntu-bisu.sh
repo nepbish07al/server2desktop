7fb631860d3ab04740f05d24b02e4d8defc00241
BISHAL POUDEL <68413032+nepbish07al@users.noreply.github.com>
2022-05-06 07:45:32 +0545

    Update ubuntu-bisu.sh

--- ubuntu-bisu.sh
+++ ubuntu-bisu.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-sudo dnf update && sudo dnf upgrade
+sudo apt update && sudo apt upgrade
 
-sudo dnf install sddm
+sudo apt install sddm
 sudo systemctl enable sddm
 sudo systemctl set-default graphical.target
 
-sudo dnf install nano
-sudo dnf install git
+sudo apt install nano
+sudo apt install git
 mkdir ~/.config
 cp bg.jpg ~/.config
 ls dotconfig/
 mv dotconfig/* ~/.config/
-sudo dnf install bspwm kitty picom polybar rofi sxhkd thunar
+sudo apt install bspwm kitty picom polybar rofi sxhkd thunar
 cat .xinit
 mv .* ~
 
-cd rpm-packages/
-ls
-sudo dnf install ocs
-sudo dnf install ocs-url-3.1.0-1.fc20.x86_64.rpm
+#cd rpm-packages/
+#ls
+#sudo dnf install ocs
+#sudo dnf install ocs-url-3.1.0-1.fc20.x86_64.rpm
 
 sudo nano /etc/sddm.conf
 echo 'set up your login page, login session=bspwm and user=YOUR_USERNAME'
 
-sudo dnf install fontawesome-fonts fontawesome-fonts-web
+sudo apt install fontawesome-fonts fontawesome-fonts-web
 
-sudo dnf install firefox
-sudo dnf install arandar 
+sudo apt install firefox
+sudo apt install arandar 
 echo 'OPEN ARANDAR AND SET your Resolution and save it as=def resolution'
 echo 'download nerdfonts -firecoda,meslo and make put them in ".fonts" directory and unzip those'
 echo 'install network manager and install wireless driver'
